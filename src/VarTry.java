69bf4eb53e924cda5fef2d6f59c1ee3443b7a51c
raphzam <raphaeljasonzamora@gmail.com>
2020-06-15 17:16:32 -0400

    Variables Project

---
+++ src/VarTry.java
@@ -0,0 +1,14 @@
+public class VarTry {
+    public static void main(String[] args) {
+
+        //declaring and assigning values to variables
+        int score = 50;
+        double price = 24.99;
+        String name = "Bobby Johnson";
+
+        //printing statements while calling variables
+        System.out.println("Test Score intialized is: " + score);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + name);
+    }
+}
